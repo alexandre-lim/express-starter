ad796ffd0a036e06ea5c153236a31c9554ed34fb
Alexandre Lim <alexandre.lim@abbeal.com>
2021-01-18 22:26:28 +0100

    chore(configuration): add eslint-plugin-import and other config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,12 @@ module.exports = {
     es6: true,
     jest: true,
   },
-  extends: ['eslint:recommended', 'eslint-config-prettier'],
+  extends: [
+    'eslint:recommended',
+    'eslint-config-prettier',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 const isTest = String(process.env.NODE_ENV) === 'test';
 
 module.exports = {
-  presets: [['@babel/preset-env', { modules: isTest ? 'commonjs' : false }]],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: isTest ? 'commonjs' : false,
+        targets: {
+          node: 'current', // to use async/await
+        },
+      },
+    ],
+  ],
 };
