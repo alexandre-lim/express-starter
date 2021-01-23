d265e30800b70a4a4f0eb176822a8d00d9700191
Alexandre Lim <alexandre.lim@abbeal.com>
2021-01-23 16:53:10 +0100

    refactor(webpack): change comments

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,11 @@ module.exports = {
   mode: 'development',
   target: 'node',
   node: {
-    // Need this when working with express, otherwise the build fails
-    __dirname: false, // if you don't put this is, __dirname
-    __filename: false, // and __filename return blank or /
+    // Keep Node.js behavior. More on this => https://webpack.js.org/configuration/node/
+    __dirname: false,
+    __filename: false,
   },
-  externals: [nodeExternals()], // Need this to avoid error when working with Express
+  externals: [nodeExternals()], // ignore all modules in node_modules folder
   entry: './src/server.js',
   output: {
     filename: '[name].bundle-server.js',
